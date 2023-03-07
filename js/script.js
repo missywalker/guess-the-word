5aa333e55d43bc82efe172374051e3d6da09e334
Melissa W <118938176+missywalker@users.noreply.github.com>
2023-03-06 21:19:48 -0500

    Showed guessed letters, updated word in progress, check if player won.

--- js/script.js
+++ js/script.js
@@ -81,5 +81,42 @@ const makeGuess = function (guess) {
     } else {
        guessedLetters.push(guess);
        console.log(guessedLetters);
+       showGuessedLetters();
+       updateWordInProgress(guessedLetters);
     }
+};
+
+const showGuessedLetters = function () {
+    //cleared list first
+    guessedLettersElement.innerHTML = "";
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersElement.append(li);
+    }
+};
+
+const updateWordInProgress = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const revealWord = [];
+    for (const letter of wordArray) {
+        if (guessedLetters.includes(letter)) {
+        revealWord.push(letter.toUpperCase());
+        } else {
+            revealWord.push("●");
+        }
+    }
+  
+    wordInProgress.innerText = revealWord.join("");
+   
+    ifPlayerWon();
+};
+
+const ifPlayerWon = function () {
+    if (word.toUpperCase() === wordInProgress.innerText)  {
+        playerMessages.classList.add("win");
+        playerMessages.innerHTML = `<p class= "highlight">You guessed correct the word! Congrats!</p>`
+    }
+
 };
